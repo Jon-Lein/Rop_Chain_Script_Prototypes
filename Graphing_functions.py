569a0584b88ed245de9e9f07643475b601fa1420
Jon Lein <34145809+Jon-Lein@users.noreply.github.com>
2020-03-17 12:42:57 -0500

    Update Graphing_functions.py

--- Graphing_functions.py
+++ Graphing_functions.py
@@ -1,33 +1,44 @@
 import matplotlib.pyplot as plt
 import numpy as np
-from ROP_Counting_Functions import count_register_use, prepare_gadgets
+import collections
+from ROP_Counting_Functions import count_register_use, prepare_gadgets, count_derefrenced_registers
+
+# --------------------
+# ---setup graphing---
+# --------------------
+
+def graph_gadgets(gadget_list, title):
+	x = []
+
+	for i in gadget_list.keys():
+		x.append(i) 
+
+	objects = x
+	y_pos = np.arange(len(objects))
+	performance = gadget_list.values()
+
+	plt.bar(y_pos, performance, align='center', alpha=0.5)
+	plt.xticks(y_pos, objects)
+	plt.ylabel('Times Used')
+	plt.title(title)
+
+	# add labels to height of bars
+	for i,v in enumerate(performance):
+		plt.text(i-0.2, v+0.1, str(v))
+
+	plt.show()
+
 
 # -----------------
 # ---Process Rop---
 # -----------------
 
-gad_list = prepare_gadgets("C:\\Users\\User\\Desktop\\Rop_testing\\Gadget_sets\\arduino.txt")
+gad_list = prepare_gadgets("C:\\Users\\User\\Desktop\\Rop_testing\\Gadget_sets\\one_variable.txt")
 
-c = count_register_use(gad_list)
+a = count_register_use(gad_list)
+b = count_derefrenced_registers(gad_list, 's')
+c = count_derefrenced_registers(gad_list, 'd')
+graphs = [a,b,c]
 
-print(c)
-# exit()
-
-# --------------------
-# ---setup graphing---
-# --------------------
-x = []
-
-for i in c.keys():
-	x.append(i) 
-
-objects = x
-y_pos = np.arange(len(objects))
-performance = c.values()
-
-plt.bar(y_pos, performance, align='center', alpha=0.5)
-plt.xticks(y_pos, objects)
-plt.ylabel('Usage')
-plt.title('Register Usage for Arduino')
-
-plt.show()
+for i in graphs:
+	graph_gadgets(i, str(i))
