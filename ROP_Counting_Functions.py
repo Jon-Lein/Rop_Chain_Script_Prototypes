7d2e8558c6a6e43be19ead961ef4db8c188a4f74
Jon Lein <34145809+Jon-Lein@users.noreply.github.com>
2020-03-16 19:59:21 -0500

    Update ROP_Counting_Functions.py
    -Change to saving count in count_register_use to a dictionary

--- ROP_Counting_Functions.py
+++ ROP_Counting_Functions.py
@@ -1,3 +1,7 @@
+# -------------------
+# -----FUNCTIONS-----
+# -------------------
+
 def prepare_gadgets(ROP_file):
 	f = open(ROP_file, "r")
 	gadgets = f.read()
@@ -17,13 +21,14 @@ def prepare_gadgets(ROP_file):
 	return preped_gadgets
 
 
-def count_register_use(reg_array, register):
-	count = 0
-	for i in reg_array: # search through each gadget
-		for j in i[1]:  # search through each instruction in gadget
-			if j.find(register) >= 0:
-				count += 1
-	return count
+def count_register_use(g_array):
+	registers = {'eax': 0, 'ebx':0, 'ecx':0, 'edx':0, 'edi':0, 'esi':0, 'esp':0, 'ebp':0}
+	for r in registers:
+		for i in g_array: # search through each gadget
+			for j in i[1]:  # search through each instruction in gadget
+				if j.find(r) >= 0:
+					registers[r] += 1
+	return registers
 
 
 def count_derefrenced_registers(g_array, register, s_or_d):
@@ -42,13 +47,31 @@ def count_derefrenced_registers(g_array, register, s_or_d):
 	return count
 
 
+def count_math_ops(g_array):
+	registers = {}
+	operations = []
+
+
+# --------------
+# -----MAIN-----
+# --------------
+
 regs = ['eax', 'ebx', 'ecx', 'edx', 'esi', 'edi', 'ebp', 'esp']
 
-gadget_list = prepare_gadgets('server.results.txt')
+path = "C:\\Users\\User\\Desktop\\Rop_testing\\Gadget_sets\\"
+file_name = "arduino.txt"
+
+gadget_list = prepare_gadgets(path + file_name)
 
 print("---All Register Usage---")
-for r in regs:
-	print(str(r) + " " + str(count_register_use(gadget_list, r)))
+
+c = count_register_use(gadget_list)
+for i in c.keys():
+	print(i + " ---> " + str(c[i]))
+
+
+
+# print(str(r) + " " + str(count_register_use(gadget_list)))
 
 print("---Derefrenced Register Source Usage---")
 for r in regs:
